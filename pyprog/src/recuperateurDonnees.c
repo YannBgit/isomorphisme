f13d17d36d3221b00a82e25bbef8c567f8b8b560
Emeric <emeric.gouy@gmail.com>
2023-01-25 01:35:53 +0100

    recuperateurDonnees : fix filename buffer overflow

--- pyprog/src/recuperateurDonnees.c
+++ pyprog/src/recuperateurDonnees.c
@@ -163,8 +163,8 @@ char *recupererNomFichier(char *dir)
 void extraireMolecules(FILE *F)
 {
 	FILE *dest;
-    char filename[50];
-    char name[1000];
+    char filename[1536];
+    char name[1536];
     //char buf[1000] = "";
 
     char *line = NULL;
@@ -189,6 +189,7 @@ void extraireMolecules(FILE *F)
 	   	while(1)
 		 	{
 				// Récupération de la ligne
+				printf("file %d\n", i);
 				n = 0;
 				if(line) free(line);
 	      n = custom_getline(&line, &n, F);
@@ -209,8 +210,11 @@ void extraireMolecules(FILE *F)
 
 	        if(strcmp(line, "> <ChEBI Name>\n") == 0)
 					{
+						fputs(line, dest);
+						printf("found name\n");
 						m = 0;
 						m = custom_getline(&nom, &m, F);
+						printf("%s", nom);
 						fputs(nom, dest);
 						nom[m-1] = 0; //strip newline from nom
 	          sprintf(name, "data/%s.sdf", nom);
